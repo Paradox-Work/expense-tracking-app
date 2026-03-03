02d54172ec45c7daadbb1885ef4fee46d78a347f
Paradox-Work <ipa23.a.vasiljevs@vtdt.edu.lv>
2026-03-03 16:48:01 +0200

    Database values entered # Sending Emails to Gmail Learned

--- database/migrations/2026_03_03_133859_create_budgets_table.php
+++ database/migrations/2026_03_03_133859_create_budgets_table.php
@@ -12,8 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('budgets', function (Blueprint $table) {
-            $table->id();
+           $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
+            $table->decimal('amount', 10, 2);
+            $table->integer('month');
+            $table->integer('year');
             $table->timestamps();
+
+            $table->unique(['user_id', 'category_id', 'month', 'year']);
         });
     }
 
--- database/migrations/2026_03_03_133925_create_expenses_table.php
+++ database/migrations/2026_03_03_133925_create_expenses_table.php
@@ -13,7 +13,26 @@ return new class extends Migration
     {
         Schema::create('expenses', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
+            $table->decimal('amount', 10, 2);
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->date('date');
+            $table->enum('type', ['one-time', 'recurring'])->default('one-time');
+
+            //recurring fields
+            $table->enum('recurring_frequency', ['daily', 'weekly', 'monthly', 'yearly'])->nullable();
+            $table->date('recurring_start_date')->nullable();
+            $table->date('recurring_end_date')->nullable();
+            $table->foreignId('parent_expense_id')->nullable()->constrained('expenses')->nullOnDelete();
+            $table->boolean('is_auto_generated')->default(false);
+
             $table->timestamps();
+            $table->softDeletes();
+            
+            $table->index(['user_id', 'date']);
+            $table->index(['user_id', 'category_id']);
         });
     }
 
